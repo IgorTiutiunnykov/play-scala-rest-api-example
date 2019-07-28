3b87ae0dd7e2eca89b1aed45cc5e4a70b3d2eef3
igor.tiutiunnykov <igor.tiutiunnykov@xing.com>
2019-07-28 10:45:27 +0200

    update

--- app/service/CompanyTSVReader.scala
+++ app/service/CompanyTSVReader.scala
@@ -12,23 +12,26 @@ import scala.io.Source
   class CompanyTSVReader(val fileName: String) extends CompanyReader {
 
     def readCompanies(): Seq[Company] = {
+      val source = Source.fromFile(fileName)
       for {
-        line <- Source.fromFile(fileName).getLines().toVector
-        values = line.split("\t").map(_.trim)
+        line <- source.getLines()
+        values = line.split("\t").map(_.trim).toVector
 //        cleaned_name = CompanyNameCleaner.cleaningTrailingStrategy(values(1)).toString
       } yield {
           try Company(values(0).toInt, NormalizeSupport.normalize(values(1)), NormalizeSupport.normalize(values(2)), values(3), values(4), values(5))
           catch {
             case _: ArrayIndexOutOfBoundsException => Company(values(0).toInt, NormalizeSupport.normalize(values(1)), "", "", "", "")
           }
+          finally { source.close() }
       }
     }
   }
 
   class TruthTSVReader(val fileName: String) extends TruthReader {
     def readCompaniesTruth(): Seq[CompanyTruth] = {
+      val source = Source.fromFile(fileName)
       for {
-        line <- Source.fromFile(fileName).getLines().toVector
+        line <- source.getLines().toVector
         values = line.split("\t").map(_.trim)
       } yield CompanyTruth(values(0).toInt, values(1).toInt, "")
     }
--- app/service/Readers.scala
+++ app/service/Readers.scala
@@ -1,12 +1,13 @@
 package service
 
 trait CompanyReader {
-    def readCompanies(): Seq[Company]
-  }
-  trait TruthReader {
-    def readCompaniesTruth(): Seq[CompanyTruth]
-  }
+  def readCompanies(): Seq[Company]
+}
 
-  case class Company(id: Int, company_name: String, website_url: String, foundation_year: String, city: String, country: String)
+trait TruthReader {
+  def readCompaniesTruth(): Seq[CompanyTruth]
+}
 
-  case class CompanyTruth(id_profiles: Int, id_entities: Int, name: String)
+case class Company(id: Int, company_name: String, website_url: String, foundation_year: String, city: String, country: String)
+
+case class CompanyTruth(id_profiles: Int, id_entities: Int, name: String)
